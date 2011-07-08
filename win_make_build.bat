3f9e9ac2bd4c0b0e9b1e94d44dea026867723f38
Jonne Nauha <jonne.nauha@adminotech.com>
2011-07-08 03:48:26 +0300

    Fix typos from make_build.bat

--- win_make_build.bat
+++ win_make_build.bat
@@ -4,5 +4,5 @@ md install\lib
 
 xcopy /d /y src\*.h install\include\libmumbleclient
 xcopy /d /y Mumble.pb.h install\include\libmumbleclient
-xcopy /d /y src\Debug\*d.lib install\include\lib
-xcopy /d /y src\Release\*.lib install\include\lib
+xcopy /d /y src\Debug\*d.lib install\lib
+xcopy /d /y src\Release\*.lib install\lib
